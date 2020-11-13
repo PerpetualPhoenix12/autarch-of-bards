0268c58494054f8d7f857ec923ce2454f0f9537f
PerpetualPhoenix12 <crazywuxia@gmail.com>
2020-11-13 21:19:07 +0000

    Add tug

--- src/constants/user-mapping.ts
+++ src/constants/user-mapping.ts
@@ -5,7 +5,7 @@ const users: UserMapping = {
   '182148584412676096': 'tadabanri14',                // Phoenix
   '163643866098958336': '4k8jsa5yyqsiw5xi6rz9eo6n2',  // Bill
   '201401898941677577': 'azkataphrakt',               // Wei
-  '105736344612937728': '',                           // Tug
+  '105736344612937728': 'sonlal1',                    // Tug
 }
 
 export default users;
