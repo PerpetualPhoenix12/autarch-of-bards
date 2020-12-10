349a80cb220ddc788e2cb8c84577977ac0f46919
PerpetualPhoenix12 <crazywuxia@gmail.com>
2020-12-10 15:53:02 +0000

    Update scrape-youtuber

---
+++ src/constants/users.ts
@@ -0,0 +1,11 @@
+import {UserMapping} from "user-mapping";
+
+const names: UserMapping = {
+  '259718929038049282': 'Neverfire',               
+  '182148584412676096': 'Phoenix',                
+  '163643866098958336': 'Billdoor',  
+  '201401898941677577': 'Weiqing',               
+  '105736344612937728': 'Tug',                    
+}
+
+export default names;
